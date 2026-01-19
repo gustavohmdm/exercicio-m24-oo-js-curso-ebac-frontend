c7411597d7dcfb57278e238e7ad67cf858d970b8
GustavoHMDM <gustavohmdm180506@gmail.com>
2026-01-19 17:49:50 -0300

    oo_js

---
+++ main.js
@@ -0,0 +1,25 @@
+function Promocao(comidas ,bebidas, produtos) {
+    this.comidas = comidas
+    this.bebidas = bebidas
+    this.produtos = produtos
+};
+
+const itens = new Promocao("Arroz", "Cerveja", "Alcool");
+
+console.log(itens);
+
+function Marca(comidas, bebidas, produtos) {
+    Promocao.call(this, comidas, bebidas, produtos)
+};
+
+const marcas = new Marca("Camil", "Skoll", "Coperalcool");
+
+console.log(marcas);
+
+function Precos(comidas ,bebidas, produtos) {
+    Promocao.call(this, comidas, bebidas, produtos)
+};
+
+const precos = new Precos("RS20", "R$20", "R$5");
+
+console.log(precos);
